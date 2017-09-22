53b87c668b971557c1ab0cb744e44b07503d86b7
Sergey Kozak <Si@deep-book.local>
2017-09-22 02:20:33 -0400

    Added code for numbers and "clear"

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -16,10 +16,46 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    // Outlets
+    
+    @IBOutlet weak var screenLabel: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    // Actions
+    
+    @IBAction func numberButton(_ sender: UIButton) {
+        
+        if Int(screenLabel.text!)! == 0 {
+            screenLabel.text = sender.titleLabel?.text!
+        } else {
+            screenLabel.text = screenLabel.text! + (sender.titleLabel?.text)!
+        }
     }
+    
+    @IBAction func decimalButton(_ sender: UIButton) {
+        if (Float(screenLabel.text!) != nil) {
+            screenLabel.text = screenLabel.text! + "."
+        } else {
+            screenLabel.text = screenLabel.text
+        }
+    }
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        screenLabel.text = String(0)
+    }
+    
+    @IBAction func operationButton(_ sender: UIButton) {
+    }
+    
+    
+    
+    
 
 
 }
