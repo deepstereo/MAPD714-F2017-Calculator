abe065ef33b8418bae69d9a28c9a994e98ce34c3
Sergey Kozak <Si@deep-book.local>
2017-09-24 14:25:24 -0400

    Added functions for operations

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -16,11 +16,42 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-   
+   // Variables
+    
     var screenValue = 0.0
-    var operand1 = 0.0
-    var operand2 = 0.0
+    var number1 = 0.0
+    var number2 = 0.0
     var operation = ""
+    
+    // Functions for mathematic operations
+    
+    func addition (a: Double, b: Double) -> String {
+        return String(a+b)
+    }
+    
+    func subtraction (a: Double, b: Double) -> String {
+       return String(a-b)
+    }
+    
+    func multiplication(a: Double, b: Double) -> String {
+        return String(a*b)
+    }
+    
+    func division (a: Double, b: Double) -> String {
+        if b != 0 {
+            return String(a/b)
+        } else {
+            return "Error!"
+        }
+    }
+    
+    func clear () -> String {
+        number1 = 0
+        number2 = 0
+        screenValue = 0
+        screen.text = "0"
+        return screen.text!
+    }
     
     
     // Outlets
@@ -33,8 +64,11 @@ class ViewController: UIViewController {
     
     @IBAction func clearButton(_ sender: UIButton) {
         screenValue = 0.0
-        screen.text = String(Int(screenValue))
+        number1 = 0.0
+        number2 = 0.0
+        screen.text = "0"
     }
+    
     
     // Numeric buttons
     
@@ -43,58 +77,88 @@ class ViewController: UIViewController {
         if screenValue == 0.0 {
             screen.text = sender.titleLabel?.text
             screenValue = Double(screen.text!)!
-            operand1 = Double(screen.text!)!
-            print("Operand 1 is \(operand1)")
         } else {
             screenValue = Double(screen.text!)!
             screen.text = screen.text! + (sender.titleLabel?.text)!
-            operand1 = Double(screen.text!)!
-            print("Operand 1 is \(operand1)")
         }
     }
+    
     
     // Decimal button
     // If screen value can be converted into an integer than add a "."
     
-   @IBAction func decimalButton(_ sender: UIButton) {
-    if Int(screen.text!) != nil {
-        screen.text = screen.text! + "."
-    } else {
-        print("Decimal point already there")
+    @IBAction func decimalButton(_ sender: UIButton) {
+        if Int(screen.text!) != nil {
+            screen.text = screen.text! + "."
+        } else {
+            print("Decimal point already there")
+        }
     }
-    }
+   
     
-    // Operations
+    // Operations - currently works like Equals!
     
     @IBAction func operationButton(_ sender: UIButton) {
-        operation = (sender.titleLabel?.text!)!
-        operand2 = Double(screen.text!)!
-        screenValue = 0.0
-        print("operaton \(operation), screenvalue \(screenValue), operand 1 \(operand1), operand 2 \(operand2)")
-       
+        
+        if number1 == 0 {
+            operation = (sender.titleLabel?.text)!
+            number1 = Double(screen.text!)!
+            screenValue = 0
+        } else {
+            if number2 == 0 {
+                number2 = number1
+                number1 = Double(screen.text!)!
+            } else {
+                print("already have both numbers")
+            }
+        }
+        screen.text = addition(a: number1, b: number2)
+        screenValue = 0
+        number2 = 0
     }
+    
     
     
     
     @IBAction func equalsButton(_ sender: UIButton) {
         
+        if number2 == 0.0 {
+            number2 = Double(screen.text!)!
+        
         switch operation != "" {
         case operation == "+":
-            screen.text = String(operand1 + operand2)
+            screen.text = addition(a: number1, b: number2)
             screenValue = 0.0
         case operation == "-":
-            screen.text = String(operand2 - operand1)
+            screen.text = subtraction(a: number1, b: number2)
             screenValue = 0.0
         case operation == "*":
-            screen.text = String(operand1 * operand2)
+            screen.text = multiplication(a: number1, b: number2)
             screenValue = 0.0
         case operation == "/":
-            screen.text = String(operand2 / operand1)
+            screen.text = division(a: number1, b: number2)
             screenValue = 0.0
         default:
             print(screenValue)
         }
-        
+        } else {
+            switch operation != "" {
+            case operation == "+":
+                screen.text = addition(a: number1, b: number2)
+                screenValue = 0.0
+            case operation == "-":
+                screen.text = subtraction(a: number1, b: number2)
+                screenValue = 0.0
+            case operation == "*":
+                screen.text = multiplication(a: number1, b: number2)
+                screenValue = 0.0
+            case operation == "/":
+                screen.text = division(a: number1, b: number2)
+                screenValue = 0.0
+            default:
+                print(screenValue)
+        }
+        }
     }
     
     
